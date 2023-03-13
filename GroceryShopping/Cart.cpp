aade86cdeabfa1b95740c970a91285f95ca36c56
Muhammad Uzair <muzair2002@gmail.com>
2023-03-13 18:13:55 -0400

    test

--- GroceryShopping/Cart.cpp
+++ GroceryShopping/Cart.cpp
@@ -8,13 +8,13 @@ Cart::Cart()
 Cart::~Cart()
 {
 }
-Item addItem(Item newItem, int quantity) {
+Item Cart::addItem(Item newItem, int quantity) {
 	for (int i = 0; i < quantity; i++) {
 		allItems.push_back(newItem);
 	}
 }
 
-float calculateTotal() {
+float Cart::calculateTotal() {
 	float sum;
 	for (int i = 0; i < allItems.size(); i++) {
 		sum += allItems[i].GetPrice();
